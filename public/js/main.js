ac6b2ad0af5df7fcbc7c7726db4b5b1c6b9f3158
Bachir Boulaich <bachir.boulaich@outlook.com>
2023-01-17 09:31:35 +0100

    Mise de la fonction blueTitle dans la méthode addEventListener

--- public/js/main.js
+++ public/js/main.js
@@ -8,9 +8,8 @@
 let h1 = document.querySelector('h1');
 // ### 2. Créer un programme qui met le texte du h1 en bleu
 // ### 3. Met ce programme dans une function
-function blueTitle(element){
-    element.style.color = 'blue';
-}
 // ### 4. Appelle cette function pour qu'elle soit executé
-blueTitle(h1);
 // ### 5. Met un écouteur d'événement sur le h1, qui au clique lance la function
+h1.addEventListener('click', function blueTitle(){
+    h1.style.color = 'blue';
+})
